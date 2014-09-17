83cf7657c2bc08a47cbb4e6fc7ffa4726659a6cc
carpedm20 <carpedm20@gmail.com>
2014-09-17 14:11:16 +0900

    file renaming

---
+++ nxvxr/1_data_analyze.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python2.7
+import json
+import sys, os
+import pprint
+from collections import Counter
+
+from utils import get_nxvxr_review_list
+
+VERBOSE = False
+COUNT = -1
+
+review_list = get_nxvxr_review_list(COUNT)
+
+c = Counter([review['code'] for review in review_list])
+print "# of movies with reviews : %s" % "{:,}".format(len(c))
+print "# of reviews : %s" % "{:,}".format(len(review_list))
+print "# of unique reviews : %s" % "{:,}".format(len(list(set([review['text'] for review in review_list]))))
+
+if VERBOSE:
+    print "Top Movies with the most reviews"
+    pprint.pprint(c.most_common(20))
+
+if VERBOSE:
+    user_list = [review['encrypted_id'] for review in review_list]
+    print "# of reviewer : %s" % len(set(user_list))
+
+    c = Counter(user_list)
+    pprint.pprint(c.most_common(20))
+
+if True:
+    word_list = [review['text'].split() for review in review_list]
+    word_list = [word for words in word_list for word in words]
+
+    print "# of all words : %s" % "{:,}".format(len(word_list))
+
+    c = Counter(word_list)
+    class MyPrettyPrinter(pprint.PrettyPrinter):
+        def format(self, _object, context, maxlevels, level):
+            if isinstance(_object, unicode):
+                return "'%s'" % _object.encode('utf8'), True, False
+            elif isinstance(_object, str):
+                _object = unicode(_object,'utf8')
+                return "'%s'" % _object.encode('utf8'), True, False
+            return pprint.PrettyPrinter.format(self, _object, context, maxlevels, level)
+
+    print "# of unique words : %s" % "{:,}".format(len(c))
+
+    MyPrettyPrinter().pprint(c.most_common(20))
---
+++ nxvxr/3_get_sample_data.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python2.7
+import sys
+import random
+
+if len(sys.argv) < 2:
+    train_name = "wxtchx_train.vw"
+else:
+    train_name = sys.argv[1]
+
+out_name = "sample_" + train_name
+
+with open(train_name) as trainf, open(out_name, 'w') as outf:
+    lines = trainf.readlines()
+    random.shuffle(lines)
+
+    outf.writelines(lines[:30])
